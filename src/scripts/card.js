4c990e3d0f3f138426c241c75fc4f9eea2403f43
Dmitry Egorov <bambiq@yandex.ru>
2025-02-15 20:03:42 +0300

    Fixed like button

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -17,12 +17,12 @@ export function createCard(
   cardImage.src = card.link;
   cardImage.alt = card.name;
   likeCount.textContent = card.likes.length;
-
-  const liked = card.likes.find(() => userId);
+  const liked = card.likes.some(user => user._id === userId);
 
   cardImage.addEventListener('click', () =>
     showImage(cardImage.src, cardImage.alt)
   );
+
   likeButton.addEventListener('click', () =>
     likeHandler(card._id, likeButton, likeCount)
   );
